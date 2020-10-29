dff5e9aa2c5b4bf83ca30c7068fcdce038dabda3
cielavenir <cielartisan@gmail.com>
2020-10-30 01:53:40 +0900

    fix swift

--- print.swift
+++ print.swift
@@ -1,6 +1,6 @@
 import Foundation
-print(ProcessInfo().processIdentifier)
-for _i in 0..<1000 {Thread.sleep(forTimeInterval: 0.01)}
+print(ProcessInfo.processInfo.processIdentifier)
+for _ in 0..<1000 {Thread.sleep(forTimeInterval: 0.01)}
 if let value = ProcessInfo.processInfo.environment["PATH"] {
   print(value)
 }
